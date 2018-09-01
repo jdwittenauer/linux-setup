4c6187f76d0311be39e1b7cb95d6a8cf4d8fc099
John Wittenauer <jdwittenauer@gmail.com>
2018-08-31 21:36:48 -0400

    Major update

--- setup.sh
+++ setup.sh
@@ -25,13 +25,24 @@ printf "\nexport GOPATH=\$HOME/go/workspace" >> ~/.bashrc
 printf "\nexport PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin" >> ~/.bashrc
 source ~/.bashrc
 
-# install Anaconda and additional packages
+# install Julia
 cd ~/Downloads
-wget "https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh"
-bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p $HOME/anaconda
+wget "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.0-linux-x86_64.tar.gz"
+tar xvzf julia-1.0.0-linux-x86_64.tar.gz
+mv ~/Downloads/julia-1.0.0 ~/julia
+printf "\nexport PATH=\$PATH:\$HOME/julia/bin" >> ~/.bashrc
+source ~/.bashrc
+rm -rf julia-1.0.0-linux-x86_64.tar.gz
+
+# install Anaconda
+cd ~/Downloads
+wget "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
+bash Anaconda3-5.2.0-Linux-x86_64.sh -b -p $HOME/anaconda
 printf "\nexport PATH=\$PATH:\$HOME/anaconda/bin" >> ~/.bashrc
 source ~/.bashrc
-rm Anaconda3-5.0.1-Linux-x86_64.sh
+rm Anaconda3-5.2.0-Linux-x86_64.sh
+
+# install additional packages
 conda update -y conda
 conda update -y anaconda
 conda update -y --all
@@ -47,7 +58,6 @@ conda install -y spacy
 ~/anaconda/bin/pip install celery
 ~/anaconda/bin/pip install deap
 ~/anaconda/bin/pip install fbprophet
-~/anaconda/bin/pip install flask-socketio
 ~/anaconda/bin/pip install keras
 ~/anaconda/bin/pip install hyperopt
 ~/anaconda/bin/pip install ortools
@@ -55,27 +65,24 @@ conda install -y spacy
 ~/anaconda/bin/pip install pymc3
 ~/anaconda/bin/pip install pyro-ppl
 ~/anaconda/bin/pip install pystan
-~/anaconda/bin/pip install tpot
+~/anaconda/bin/pip install xgboost
+
+# install and configure jupyter theme
+~/anaconda/bin/pip install jupyterthemes
+jt -t chesterish -fs 10 -ofs 95 -cellw 80% -lineh 130 -T
 
 # install tensor libraries
 ~/anaconda/bin/pip install --upgrade --ignore-installed setuptools
 if [ "$GPU" = "TRUE" ]
 then
-	conda install pytorch torchvision -c pytorch
-	~/anaconda/bin/pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.9.0-cp36-cp36m-linux_x86_64.whl
+	conda install -y pytorch torchvision -c pytorch
+	~/anaconda/bin/pip install tensorflow-gpu
 else
-	conda install pytorch-cpu torchvision-cpu -c pytorch
-	~/anaconda/bin/pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.9.0-cp36-cp36m-linux_x86_64.whl
+	conda install -y pytorch-cpu torchvision-cpu -c pytorch
+	~/anaconda/bin/pip install tensorflow
 fi
 
-# download and install git repos
-cd ~/git
-git clone --recursive https://github.com/dmlc/xgboost
-cd ~/git/xgboost
-make -j4
-cd python-package
-~/anaconda/bin/python setup.py develop --user
-
+# download and install custom library
 cd ~/git
 git clone https://github.com/jdwittenauer/ionyx
 cd ~/git/ionyx
@@ -83,42 +90,36 @@ cd ~/git/ionyx
 
 if [ "$GUI" = "TRUE" ]
 then
+	# install extensions for VS code
+	code --install-extension formulahendry.code-runner
+	code --install-extension formulahendry.vscode-mysql
+	code --install-extension julialang.language-julia
+	code --install-extension msjsdiag.debugger-for-chrome
+	code --install-extension ms-python.python
+	code --install-extension ms-vscode.cpptools
+	code --install-extension ms-vscode.csharp
+	code --install-extension ms-vscode.Go
+	code --install-extension PeterJausovec.vscode-docker
+	code --install-extension PKief.material-icon-theme
+	code --install-extension scala-lang.scala
+	code --install-extension vscjava.vscode-java-pack
+
 	# download additional git repos
 	cd ~/git
-	git clone https://github.com/jdwittenauer/kaggle
-	git clone https://github.com/jdwittenauer/ipython-notebooks
-	git clone https://github.com/jdwittenauer/twitter-viz-demo
 	git clone https://github.com/jdwittenauer/alpha
+	git clone https://github.com/jdwittenauer/blogit
+	git clone https://github.com/jdwittenauer/dotnet-sandbox
 	git clone https://github.com/jdwittenauer/hackerrank
-	git clone https://github.com/jdwittenauer/sandbox
+	git clone https://github.com/jdwittenauer/hadoop-training
+	git clone https://github.com/jdwittenauer/insight-net
+	git clone https://github.com/jdwittenauer/ipython-notebooks
+	git clone https://github.com/jdwittenauer/kaggle
 	git clone https://github.com/jdwittenauer/linux-setup
-
-	# install and configure PyCharm
-	# darcula theme with inconsolata font size 30
-	# install plugins
-	# configure github login
-	# set interpreter to Anaconda
-	cd ~/Downloads
-	wget "https://download-cf.jetbrains.com/python/pycharm-community-2017.3.3.tar.gz"
-	tar -xzf pycharm-community-2017.3.3.tar.gz
-	mv ~/Downloads/pycharm-community-2017.3.3 ~/pycharm
-	cd ~/pycharm/bin
-	bash pycharm.sh
-
-	# install and configure Intellij IDEA
-	# darcula theme with inconsolata font size 30
-	# install plugins
-	# configure github login
-	# configure Java and Scala SDKs
-	cd ~/Downloads
-	wget "https://download-cf.jetbrains.com/idea/ideaIC-2017.3.3.tar.gz"
-	tar -xzf ideaIC-2017.3.3.tar.gz
-	mv ~/Downloads/idea-IC-173.4301.25 ~/idea
-	cd ~/idea/bin
-	bash idea.sh
-
-	# clean up downloaded files
-	cd ~/Downloads
-	rm pycharm-community-2017.3.3.tar.gz
-	rm ideaIC-2017.3.3.tar.gz
+	git clone https://github.com/jdwittenauer/little-compiler
+	git clone https://github.com/jdwittenauer/or-tools-examples
+	git clone https://github.com/jdwittenauer/ppm-cut-detection
+	git clone https://github.com/jdwittenauer/projecteuler-solutions
+	git clone https://github.com/jdwittenauer/sandbox
+	git clone https://github.com/jdwittenauer/selforganizingmap-demo
+	git clone https://github.com/jdwittenauer/twitter-viz-demo
 fi
--- setup_admin.sh
+++ setup_admin.sh
@@ -11,64 +11,85 @@ GPU="TRUE"
 export DEBIAN_FRONTEND=noninteractive
 
 # initial setup
-cd ~
-mkdir blas
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install build-essential
-sudo apt-get -y autoremove
-sudo apt-get -y install wget
-sudo apt-get -y install git
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install build-essential
+sudo apt -y autoremove
+sudo apt -y install wget
+sudo apt -y install git
 git config --global user.name "John Wittenauer"
 git config --global user.email jdwittenauer@gmail.com
 
-# install JDK and Fortran compiler
-sudo apt-get -y install openjdk-8-jre openjdk-8-jdk
-sudo apt-get -y install gfortran
+# install JDK
+sudo apt -y install openjdk-11-jre openjdk-11-jdk
+
+# install fortran compiler
+sudo apt -y install gfortran
+
+# install OpenCV libraries
+sudo apt -y install libopencv-dev python-opencv
+
+# install MySQL
+sudo -E apt -q -y install mysql-server
+
+# install Go
+sudo apt -y install golang
 
 # install Scala
 cd ~/Downloads
-wget "http://downloads.lightbend.com/scala/2.12.4/scala-2.12.4.deb"
-sudo dpkg -i scala-2.12.4.deb
-sudo apt-get -y update
-sudo apt-get -y install scala
-rm -rf scala-2.12.4.deb
+wget "http://downloads.lightbend.com/scala/2.12.6/scala-2.12.6.deb"
+sudo dpkg -i scala-2.12.6.deb
+sudo apt -y update
+sudo apt -y install scala
+rm -rf scala-2.12.6.deb
 
 # install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get -y update
-sudo apt-get -y install sbt
+sudo apt -y update
+sudo apt -y install sbt
 
-# install Go
-sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
-sudo apt-get -y update
-sudo apt-get -y install golang
+# install Mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt -y update
+sudo apt -y install mono-devel
 
-# build OpenBLAS
-cd ~/blas
-git clone https://github.com/xianyi/OpenBLAS
-cd OpenBLAS
-sudo make FC=gfortran
-sudo make PREFIX=/usr/local install
+# install node.js
+sudo apt -y install curl
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt -y install nodejs
 
-# install OpenCV libraries
-sudo apt-get -y install libopencv-dev python-opencv
+# install Docker
+sudo apt -y install apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt -y update
+sudo apt -y install docker-ce
 
 # install Chrome
 # pin to task bar by finding via search and dragging to bar
 cd ~/Downloads
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
-sudo apt-get -y update
-sudo apt-get -y install -f
+sudo apt -y update
+sudo apt -y install -f
 printf "\nexport BROWSER=google-chrome" >> ~/.bashrc
 rm -rf google-chrome-stable_current_amd64.deb
 
 # install inconsolata font
-sudo apt-get -y install fonts-inconsolata
+sudo apt -y install fonts-inconsolata
 sudo fc-cache -fv
-sudo apt-get -y autoremove
+
+# build OpenBLAS
+cd ~
+mkdir blas
+cd blas
+git clone https://github.com/xianyi/OpenBLAS
+cd OpenBLAS
+sudo make FC=gfortran
+sudo make PREFIX=/usr/local install
 
 if [ "$GUI" = "TRUE" ]
 then
@@ -76,33 +97,38 @@ then
 	# adjust mouse speed
 	# install and configure tilda (height 100, width 100, transparency 40, always on top = FALSE)
 	# pin to task bar by finding via search and dragging to bar
-	sudo apt-get -y install tilda
+	sudo apt -y install tilda
 
-	# install and configure Atom
-	# install go-plus, go-debug, platformio-ide-terminal
+	# install VS Code
 	cd ~/Downloads
-	wget "https://atom-installer.github.com/v1.23.3/atom-amd64.deb"
-	sudo dpkg -i atom-amd64.deb
-	sudo apt-get -y update
-	sudo apt-get -y install -f
-	rm -rf atom-amd64.deb
+	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+	sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+	sudo apt -y install apt-transport-https
+	sudo apt -y update
+	sudo apt -y install code
+	sudo rm microsoft.gpg
 fi
 
 if [ "$GPU" = "TRUE" ]
 then
+	# install gcc
+	sudo apt -y install gcc-6
+	sudo apt -y install g++-6
+
 	# update GPU driver
 	sudo add-apt-repository ppa:graphics-drivers/ppa -y
-	sudo apt-get -y update
-	sudo apt-get -y upgrade
-	sudo apt-get -y autoremove
+	sudo apt -y update
+	sudo apt -y upgrade
+	sudo apt -y autoremove
 
 	# install cuda
 	cd ~/Downloads
 	wget "https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb"
 	sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 	sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
-	sudo apt-get -y update
-	sudo apt-get -y install cuda
+	sudo apt -y update
+	sudo apt -y install cuda
 	printf "\nexport CUDA_HOME=/usr/local/cuda" >> ~/.bashrc
 	printf "\nexport CUDA_ROOT=/usr/local/cuda" >> ~/.bashrc
 	printf "\nexport PATH=\$PATH:/usr/local/cuda/bin" >> ~/.bashrc
@@ -124,3 +150,5 @@ then
 	sudo rm cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 	sudo rm cudnn-9.0-linux-x64-v7.tgz
 fi
+
+sudo apt -y autoremove
